bb529b903f05f676596f220c681e274ea5981003
volthouse <lucasgaertner@arcor.de>
2013-03-20 12:53:58 -0700

    Binär zu BCD, BCD zu binär, Binär zu Ascii, Ascii zu Binär implementiert.

--- serial/serial.asm
+++ serial/serial.asm
@@ -1,10 +1,13 @@
 .include "m8def.inc"
 .include "macros.inc"
 
+.def Null	= R15								; := 0
 .def temp1  = r16                             	; Register für kleinere Arbeiten
 .def temp2	= r17
-.def char 	= r18                              	; in diesem Register wird das Zeichen an die
-                                                ; Ausgabefunktion übergeben 
+.def temp3	= r18
+.def char 	= r19                              	; in diesem Register wird das Zeichen an die
+                                                ; Ausgabefunktion übergeben
+
 .equ F_CPU 	= 16000000                          ; Systemtakt in Hz
 .equ BAUD  	= 9600                              ; Baudrate
 
@@ -39,13 +42,15 @@ CmdBuffer: .BYTE 10 							; Usart Empfangspuffer
 ; Initialisierung
 ;
 Reset:
+	clr		Null
+
     ; Stackpointer initialisieren
  
     ldi     temp1, HIGH(RAMEND)
     out     SPH, temp1
     ldi     temp1, LOW(RAMEND)
     out     SPL, temp1
- 
+
     ; Baudrate einstellen
  
     ldi     temp1, HIGH(UBRR_VAL)
@@ -62,12 +67,12 @@ Reset:
 	sbi     UCSRB, RXCIE                ; Interrupt bei Empfang
     sbi     UCSRB, RXEN                 ; RX (Empfang) aktivieren
     
-	vectorR	x, CmdBuffer				; Zeiger auf Command Buffer setzten
+	Vector	x, CmdBuffer				; Zeiger auf Command Buffer setzten
 	clr		char						; Zeichen löschen
     sei									; Interrupts global aktivieren
 
 
-	vector	z, TxtStart					; Start Text ausgeben
+	ZTab	TxtStart, Null					; Start Text ausgeben
 	rcall	TxtOut	
 
 ;
@@ -152,8 +157,8 @@ SerialSync_loop:
 ; Befehle die über den Uart empfangen wurden bearbeiten
 ;
 Cmd:
-	vector	z, CmdTable
-	vectorR	x, CmdBuffer
+	ZTab	CmdTable, Null
+	Vector	x, CmdBuffer
 CmdCheckChar:	
 	lpm		temp1, z+
 	ld		temp2, x+
@@ -166,46 +171,197 @@ CmdCheckChar:
 	rjmp	CmdCheckChar
 
 CmdMatch:
-	vectorR	x, CmdBuffer
+	Vector	x, CmdBuffer
 	lpm     temp1,z+                    ; Low Byte laden und Pointer erhöhen
     lpm     zh,z                        ; zweites Byte laden
     mov     zl,temp1                    ; erstes Byte in Z-Pointer kopieren
     ijmp
 
 CmdNext:
-	vectorR	x, CmdBuffer
+	Vector	x, CmdBuffer
 	adiw	zl, 5
 	rjmp	CmdCheckChar
 
 CmdExit:
-	vectorR	x, CmdBuffer
+	Vector	x, CmdBuffer
 	ret;
 
 ;
 ; Befehle
 ;
 Cmd1:
-	vector z, Txt1
+	ZTab 	Txt1, Null
 	rcall	TxtOut
 	ret
 
 Cmd2:
-	vector z, Txt2
+	ZTab 	Txt2, Null
 	rcall	TxtOut
 	ret
 
 Cmd3:
-	vector z, TxtStart
+	ZTab 	TxtStart, Null
 	rcall	TxtOut
 	ret
 
-;
-; Tabelle der Befehle
-;
+Cmd4:
+	ldi		temp1, 42
+	rcall	Bin2Asc8
+	mov		char, temp2
+	rcall	SerialOut
+	mov		char, temp1
+	rcall	SerialOut
+	ret
+
+
+
+
+;***************************************************************************
+;*
+;* "Bin2BCD8" - 8-bit Binary to BCD conversion
+;*
+;* This subroutine converts an 8-bit number (temp1) to a 2-digit
+;* BCD number (temp1:temp2).
+;*
+;* Number of words	:6 + return
+;* Number of cycles	:5/50 (Min/Max) + return
+;* Low registers used	:None
+;* High registers used  :2 (temp1/temp2,temp3)
+;*
+;* Included in the code are lines to add/replace for packed BCD output.	
+;*
+;***************************************************************************
+
+;***** Subroutine Register Variables
+
+;temp1	8-bit binary value
+;temp1	BCD result MSD
+;temp2	BCD result LSD
+
+;***** Code
+
+Bin2bcd8:
+	clr		temp2			;clear result MSD
+bBCD8_1:
+	subi	temp1,10	;input = input - 10
+	brcs	bBCD8_2		;abort if carry set
+	inc		temp2		;inc MSD
+;---------------------------------------------------------------------------
+; Replace the above line with this one
+; for packed BCD output				
+;	subi	temp2,-$10 	;temp2 = temp2 + 10
+;---------------------------------------------------------------------------
+	rjmp	bBCD8_1		;loop again
+bBCD8_2:
+	subi	temp1,-10	;compensate extra subtraction
+;---------------------------------------------------------------------------
+; Add this line for packed BCD output
+;	add	temp1,temp2	
+;---------------------------------------------------------------------------	
+	ret
+
+
+;***************************************************************************
+;*
+;* "Bin2Ascii8" - 8-bit Binary to Ascii conversion
+;*
+;* This subroutine converts an 8-bit number (temp1) to a 2-digit
+;* Ascii number (temp3:temp2).
+;*
+;* Number of words	:6 + return
+;* Number of cycles	:5/50 (Min/Max) + return
+;* Low registers used	:None
+;* High registers used  :2 (temp1/temp2,temp3)
+;*
+;* Included in the code are lines to add/replace for packed Ascii output.	
+;*
+;***************************************************************************
+
+;***** Subroutine Register Variables
+
+; temp1	8-bit binary value
+; temp2	Ascii result MSD
+; temp3	Ascii result LSD
+
+;***** Code
+
+Bin2Asc8:
+	rcall	Bin2BCD8
+	ldi		temp3, '0'
+	add		temp1, temp3
+	add		temp2, temp3
+	ret
+
+
+
+Asc2Bin8:
+	ldi		temp3, '0'
+	sub		temp1, temp3
+	sub		temp2, temp3
+	rcall	BCD2bin8
+	ret
+
+;***************************************************************************
+;*
+;* "BCD2bin8" - BCD to 8-bit binary conversion
+;*
+;* This subroutine converts a 2-digit BCD number (temp2:temp1) to an
+;* 8-bit number (temp1).
+;*
+;* Number of words	:4 + return
+;* Number of cycles	:3/48 (Min/Max) + return
+;* Low registers used	:None
+;* High registers used  :2 (temp1/temp1,temp2)	
+;*
+;* Modifications to make the routine accept a packed BCD number is indicated
+;* as comments in the code. If the modifications are used, temp2 shall be
+;* loaded with the BCD number to convert prior to calling the routine.
+;*
+;***************************************************************************
+
+;***** Subroutine Register Variables
+
+; temp1	binary result
+; temp1	lower digit of BCD input
+; temp2	higher digit of BCD input
+
+;***** Code
+
+BCD2bin8:
+;--------------------------------------------------------------------------
+;| For packed BCD input, add these two lines
+;|	mov	temp1,temp2	;copy input to result
+;|	andi	temp1,$0f	;clear higher nibble of result
+;--------------------------------------------------------------------------
+
+BCDb8_0:
+	subi	temp2,1		;temp2 = temp2 - 1
+;--------------------------------------------------------------------------
+;| For packed BCD input, replace the above
+;| line with this
+;|	subi	temp2,$10	;MSD = MSD - 1
+;--------------------------------------------------------------------------
+
+	brcs	BCDb8_1		; if carry not set
+
+	subi	temp1,-10	; result = result + 10
+	rjmp	BCDb8_0		; loop again
+BCDb8_1:
+	ret			;else return
+
+
+
+;***************************************************************************
+;*
+;* Tabelle der Befehle
+;*
+;***************************************************************************
+
 CmdTable:
 	.db "set",ccr .dw Cmd1
 	.db "del",ccr .dw Cmd2
 	.db "clr",ccr .dw Cmd3
+	.db "num",ccr .dw Cmd4
 	.db	cnull
 
 ;
